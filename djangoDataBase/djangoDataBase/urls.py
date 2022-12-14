2363c3bf5c106241b67ba2b02a0c8e94d9cdefa7
tabbee <104757339+tabbep0h@users.noreply.github.com>
2022-12-14 11:10:01 +0300

    Delete djangoDataBase directory

--- djangoDataBase/djangoDataBase/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from djangoDataBaseAPP import views
-
-urlpatterns = [
-    path('createPost', views.CreatePost),
-    path('admin/', admin.site.urls),
-    path('news', views.index, name='news')
-]
--- djangoDataBase/djangoDataBaseAPP/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import PostList
-
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = PostList
-        fields = '__all__'
